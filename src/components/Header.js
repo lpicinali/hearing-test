46a797e592fb01dbe15736e510c8ba2216350bf8
Alexander Wallin <office@alexanderwallin.com>
2017-08-10 22:24:13 +0100

    Removes header navigation stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { T } from 'lioness'
-
-import { AppUrl } from 'src/constants.js'
 
 export default function Header() {
   return (
     <header>
       <T>3D Tune-In Hearing Test</T>
-      <nav>
-        <Link to={AppUrl.HOME}>
-          <T>Home</T>
-        </Link>
-        <Link to={AppUrl.CALIBRATION}>
-          <T>Calibration</T>
-        </Link>
-      </nav>
     </header>
   )
 }
