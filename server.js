eec8af03c67d375b8b379eb8f4102be6efa10f3c
Alexander Wallin <office@alexanderwallin.com>
2017-09-12 16:38:28 +0100

    Adds dead /results/download page

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 // API
 app.use('/api', api)
 
+// PDF download
+app.get('/results/download', (req, res) => {
+  res.json({ status: 'ok' })
+})
+
 // Web app
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
