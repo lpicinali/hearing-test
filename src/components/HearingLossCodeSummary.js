597e3fdbf28567b633ab594c5d4c855930323398
Alexander Wallin <office@alexanderwallin.com>
2017-11-13 23:07:00 +0100

    Styles hearing loss code text summarys

--- src/components/HearingLossCodeSummary.js
+++ src/components/HearingLossCodeSummary.js
@@ -1,9 +1,16 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { T } from 'lioness'
+import styled from 'styled-components'
 
 import { isIndicatingHearingLoss } from 'src/evaluation.js'
+import { GRAY } from 'src/styles/colors.js'
 import { P } from 'src/styles/elements.js'
+
+const SmallP = styled(P)`
+  font-size: 14px;
+  color: ${GRAY};
+`
 
 /**
  * Hearing Loss Code Summary
@@ -18,34 +25,34 @@ class HearingLossCodeSummary extends PureComponent {
 
     return isIndicatingHearingLoss(code) === true ? (
       <div>
-        <P>
+        <SmallP>
           <T>
             This test indicates that you may have a hearing loss and that you
             could benefit from wearing hearing aids.
           </T>
-        </P>
-        <P>
+        </SmallP>
+        <SmallP>
           <T>
             This test cannot replicate a clinical hearing assessment, so we
             recommend that you visit a Hearing Care Professional for a more
             accurate assessment and advice.
           </T>
-        </P>
+        </SmallP>
       </div>
     ) : (
       <div>
-        <P>
+        <SmallP>
           <T>
             This test indicates that your hearing is at a good level and that
             hearing aids would not be suitable for you at this time.
           </T>
-        </P>
-        <P>
+        </SmallP>
+        <SmallP>
           <T>
             This test cannot replicate a clinical hearing assessment, please
             visit a Hearing Care Professional for a more accurate hearing test.
           </T>
-        </P>
+        </SmallP>
       </div>
     )
   }
